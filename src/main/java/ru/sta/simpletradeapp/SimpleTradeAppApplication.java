a9e67f9460b43aafe37a0db8daeb130bd7b649a8
Alexkm07 <alexkm@inbox.ru>
2021-05-30 10:59:02 +0300

    Initial commit

---
+++ src/main/java/ru/sta/simpletradeapp/SimpleTradeAppApplication.java
@@ -0,0 +1,13 @@
+package ru.sta.simpletradeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTradeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTradeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sta/simpletradeapp/SimpleTradeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sta.simpletradeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTradeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
